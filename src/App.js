532a0a697b3c746a9464a50fe21af14183bfc582
shubham-1903 <shubhamkr7677@gmail.com>
2020-09-25 16:29:56 +0530

    Added Brower route app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Error from "./Error";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Switch>
+				<Route exact path='/' component={About}/>
+        <Route path="/contact" component={Contact} />
+        <Route component={Error} />
+			</Switch>
+			{/* <About/>
+      <Contact/> */}
+		</>
+	);
 }
 
 export default App;
---
+++ src/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div>
+      OOppss.. Page Not Found(404)
+    </div>
+  )
+}
+
+export default Error
